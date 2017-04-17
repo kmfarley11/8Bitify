f8d27196cb23f65817488c68c4137fd1d8ee2e88
okeltw <okeltw@mail.uc.edu>
2017-04-17 10:41:14 -0400

    Slight changes to command line parsing

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main(argv):
     except:
         print("usage: python3 8bitify.py -i <wavfile> -m <module> <moduleopts>")
         sys.exit(2)
-    
+
     # Init Defaults
     inputfile = "def"
     module = "def"
@@ -23,14 +23,14 @@ def main(argv):
 
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            bitify_help()
+            print(HELP)
             sys.exit()
         elif opt in ("-i", "--input"):
             if arg == "":
                 print("No input file provided.")
                 sys.exit(2)
             inputfile = arg
-        elif opt in ("-m", "--module") and arg in MODULES: 
+        elif opt in ("-m", "--module") and arg in MODULES:
             module = MODULES[arg]
             is_module = True
         elif opt in ("-f", "--frequency"):
@@ -45,32 +45,31 @@ def main(argv):
                 print("Amplitude provided with no module to use.")
         else:
             print("Unrecognized input:", opt, "with argument", arg)
-    
-    print(inputfile)
-    print(module)
-    print(is_module)
-    print(frequency)
-    print(amplitude)
 
-def bitify_help():
-    print("8Bitify: A sound file experimentation program")
-    print("This package will take a .WAV file, modify it using any provided modules, and play it back to the user.")
-    print("This is intended to provide a method of experimenting with digital music.")
-    print()
-    print("Usage: ")
-    print("------")
-    print("python3 8bitify.py -i <wavfile> -m <module> <moduleopts>")
-    print()
-    print("  WAV File*:                 The path to the sound file, saved in .WAV format")
-    print("  Module:                    The alteration function to run on the .WAV file, e.g. Square")
-    # TODO: build this from the module
-    print("     Square:                 Superimpose a square wave across the entire .WAV")
-    print("         Frequency (-f)      The frequency, in Hz, of the square wave.")
-    print("         Amplitude (-a)      The amplitude of the square wave.")
-    print()
-    print("Supported Modules: ")
-    for entry in MODULES:
-        print(" ", entry)
+""" Some baseline for an eventual interactive console
+def interactive():
+    while True:
+        line = input(">>> ")
+        exp = parse(line)
+"""
+
+HELP = """
+8Bitify: A sound file experimentation program
+This package will take a .WAV file, modify it using any provided modules,
+and play it back to the user.
+This is intended to provide a method of experimenting with digital music.
+
+Usage:
+------
+python3 8bitify.py -i <wavfile> -m <module> <moduleopts>
+
+  WAV File*:                 The path to the sound file, saved in .WAV format
+  Module:                    The alteration function to run on the .WAV file, e.g. Square
+
+    Square:                 Superimpose a square wave across the entire .WAV
+         Frequency (-f)      The frequency, in Hz, of the square wave.
+         Amplitude (-a)      The amplitude of the square wave.
+"""
 
 if __name__ == "__main__":
     main(sys.argv[1:])
