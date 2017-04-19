f1df414cddf52f85227a6087c7781cc382adc615
farleykm <farleykm@mail.uc.edu>
2017-04-19 15:49:32 -0400

    #5 implement 8bitify via numpy functions

--- source/matrix_ops.py
+++ source/matrix_ops.py
@@ -20,23 +20,32 @@ testGuitarFile = "ThuMar2302_40_45UTC2017.wav"
 testWavFileOut = "testWavOut.wav"
 
 def eight_bitify(data):
-	""" takes numpy matrix of audio data, returns modded numpy data
+    
+    """ takes numpy matrix of audio data, returns modded numpy data
 
-	>>> t = np.array([1, 2, -5, 4, 5, 6, 120, 355, -500])
-	>>> t
-	array([   1,    2,   -5,    4,    5,    6,  120,  355, -500])
-	>>> t.shape
-	(9,)
-	>>> result = eight_bitify(t)
-	>>> result.shape
-	(9,)
-	>>> result
-	array([   1,    2,   -5,    4,    5,    6,  120,  355, -500])
+    >>> t = np.array([1, 2, -5, 4, 5, 6, 120, 355, -500])
+    >>> t
+    array([   1,    2,   -5,    4,    5,    6,  120,  355, -500])
+    >>> t.shape
+    (9,)
+    >>> result = eight_bitify(t)
+    >>> result.shape
+    (9,)
+    >>> result
+    array([  -1.81102362,    1.55511811,   -5.17716535,    1.55511811,
+              4.92125984,    4.92125984,  119.37007874,  355.        , -500.        ])
 
-	"""
-	moddedData = np.array(data)
+    """
 
-	# do 8bit rectification here...
-	# probably will use np.where, or maybe use a lambda function, math function, or a cast
+    # get max / min, then set discrete points for resolution depicted
+    moddedData = np.array(data)
+    hi = np.max(data)
+    lo = np.min(data)
+    bins = np.linspace(lo, hi, num=255)
 
-	return moddedData
+    # discretize the data into our new resolution set of points (less noticeable for small data...)
+    bidxs = np.digitize(moddedData, bins, right=False)
+    moddedData = bins[bidxs-1]
+
+    # return discretized data
+    return moddedData
